c6d242404f05a356cdaa319341b244372d329fe6
Hamza <Hamza@email.com>
2023-04-24 23:13:22 -0700

    first prototype of our printf

---
+++ _printf.c
@@ -0,0 +1,54 @@
+#include"main.h"
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<stdarg.h>
+
+/**
+* _printf - our printf functionnn
+* @format: string contain specifier form
+* Return: num of char printed
+*/
+
+int _printf(const char *format, ...)
+{
+int num_prt_char = 0, num = 0;
+va_list arg;
+va_start(arg, format);
+while (*format)
+{
+if (*format == '%')
+{ format++;
+switch (*format)
+{
+case 'c':
+num = va_arg(args, int);
+write(STDOUT_FILENO, &num, 1);
+num_prt_char++;
+break;
+case 's':
+str = va_arg(args, char *);
+if (str == NULL)
+str = "(null)";
+while (*str)
+{
+write(STDOUT_FILENO, str, 1);
+str++;
+num_prt_char++; }
+break;
+case '%':
+write(STDOUT_FILENO, "%", 1);
+num_prt_char++;
+break;
+default:
+write(STDOUT_FILENO, "%", 1);
+write(STDOUT_FILENO, &(*format), 1);
+num_prt_char += 2;
+break; }}
+else
+{
+write(STDOUT_FILENO, &(*format), 1);
+num_prt_char++; }
+format++; }
+va_end(arg);
+return (num_prt_char); }
